a94418be4049aae970e783cc0d6b9bc46a6de293
Patryk czeczot <patryk.czeczot@nokia.com>
2016-10-30 00:34:04 +0200

    Added bootstrap.

--- lessons_timetabler/urls.py
+++ lessons_timetabler/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^data/', include('data.urls')),
+    url(r'^data/', include('data.urls'),
+    url(r'^$', include('data.urls')),
 ]
